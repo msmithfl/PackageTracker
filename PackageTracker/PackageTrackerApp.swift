d6bc71b9399f1b32449eec267a064892103b7224
Matthew Smith <msmithfl@icloud.com>
2023-07-17 14:06:48 -0400

    Initial Commit

---
+++ PackageTracker/PackageTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PackageTrackerApp.swift
+//  PackageTracker
+//
+//  Created by Matthew Smith on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct PackageTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
