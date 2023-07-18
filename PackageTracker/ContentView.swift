922f447b6c40f4f83d52ef2209abdad34b6aba08
Matt Smith <msmithfl@icloud.com>
2023-07-18 11:52:46 -0400

    test12345

--- PackageTracker/ContentView.swift
+++ PackageTracker/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Testing1234")
+            Text("Testing12345")
         }
         .padding()
     }
